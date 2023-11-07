89c8a94db63c9106f12ebf7a6ff8b21c84a21e3f
eaahxn <alzl053@naver.com>
2023-11-07 14:22:06 +0900

    sub 추가

--- js/script.js
+++ js/script.js
@@ -2,76 +2,79 @@ gsap.registerPlugin(ScrollTrigger);
 
 const pageContainer = document.querySelector(".container");
 
-/* SMOOTH SCROLL */
 const scroller = new LocomotiveScroll({
-  el: pageContainer,
-  smooth: true
+    el: pageContainer,
+    smooth: true
 });
 
 scroller.on("scroll", ScrollTrigger.update);
 
 ScrollTrigger.scrollerProxy(pageContainer, {
-  scrollTop(value) {
-    return arguments.length
-      ? scroller.scrollTo(value, 0, 0)
-      : scroller.scroll.instance.scroll.y;
-  },
-  getBoundingClientRect() {
-    return {
-      left: 0,
-      top: 0,
-      width: window.innerWidth,
-      height: window.innerHeight
-    };
-  },
-  pinType: pageContainer.style.transform ? "transform" : "fixed"
-});
-
-////////////////////////////////////
-////////////////////////////////////
-window.addEventListener("load", function () {
-  let pinBoxes = document.querySelectorAll(".pin-wrap > *");
-  let pinWrap = document.querySelector(".pin-wrap");
-  let pinWrapWidth = pinWrap.offsetWidth;
-  let horizontalScrollLength = pinWrapWidth - window.innerWidth;
-
-  // Pinning and horizontal scrolling
-
-  gsap.to(".pin-wrap", {
-    scrollTrigger: {
-      scroller: pageContainer, //locomotive-scroll
-      scrub: true,
-      trigger: "#sectionPin",
-      pin: true,
-      // anticipatePin: 1,
-      start: "top top",
-      end: pinWrapWidth
+    scrollTop(value) {
+        return arguments.length
+            ? scroller.scrollTo(value, 0, 0)
+            : scroller.scroll.instance.scroll.y;
     },
-    x: -horizontalScrollLength,
-    ease: "none"
-  });
+    getBoundingClientRect() {
+        return {
+            left: 0,
+            top: 0,
+            width: window.innerWidth,
+            height: window.innerHeight
+        };
+    },
+    pinType: pageContainer.style.transform ? "transform" : "fixed"
+});
 
-  ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
-
-  ScrollTrigger.refresh();
+window.addEventListener('popstate', (event) => {
+    var scrolltop = localStorage.getItem("top");
+    if (scrolltop != undefined && scrolltop > 0) {
+        gsap.to(window, { duration: 0, scrollTo: { y: scrolltop, autoKill: true } });
+    }
 });
 
 
+window.addEventListener("load", function () {
+    let pinBoxes = document.querySelectorAll(".pin-wrap > *");
+    let pinWrap = document.querySelector(".pin-wrap");
+    let pinWrapWidth = pinWrap.offsetWidth;
+    let horizontalScrollLength = pinWrapWidth - window.innerWidth;
+
+    // Pinning and horizontal scrolling
+
+    gsap.to(".pin-wrap", {
+        scrollTrigger: {
+            scroller: pageContainer, //locomotive-scroll
+            scrub: true,
+            trigger: "#sectionPin",
+            pin: true,
+            // anticipatePin: 1,
+            start: "top top",
+            end: pinWrapWidth
+        },
+        x: -horizontalScrollLength,
+        ease: "none"
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
+
+    ScrollTrigger.refresh();
+});
 
 const link = document.querySelectorAll(".hover_link");
 const animateit = function (e) {
-  const img = this.querySelector(".hover");
-  const { offsetX: x, offsetY: y } = e;
-  const { offsetWidth: width, offsetHeight: height } = this;
-  const move = 20;
-  const xMove = (x / width) * (move * 2) - move;
-  const yMove = (y / height) * (move * 2) - move;
+    const hover = this.querySelector(".hover");
+    const { offsetX: x, offsetY: y } = e;
+    const { offsetWidth: width, offsetHeight: height } = this;
+    const move = 20;
+    const xMove = (x / width) * (move * 2) - move;
+    const yMove = (y / height) * (move * 2) - move;
 
-  img.style.transform = `translate(${xMove}px, ${yMove}px)`;
+    hover.style.transform = `translate(${xMove}px, ${yMove}px)`;
 
-  if (e.type === "mouseleave") {
-    img.style.transform = "";
-  }
+    if (e.type === "mouseleave") {
+        hover.style.transform = "";
+    }
 };
 
 link.forEach((t) => t.addEventListener("mousemove", animateit));
