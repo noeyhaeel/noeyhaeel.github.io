7efb6741d8d71b508c25e227e55c30630a9b1a45
eaahxn <alzl053@naver.com>
2023-11-07 17:39:50 +0900

    수정

--- js/script.js
+++ js/script.js
@@ -1,55 +1,22 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const pageContainer = document.querySelector(".container");
-
-const scroller = new LocomotiveScroll({
-    el: pageContainer,
-    smooth: true
-});
-
-scroller.on("scroll", ScrollTrigger.update);
-
-ScrollTrigger.scrollerProxy(pageContainer, {
-    scrollTop(value) {
-        return arguments.length
-            ? scroller.scrollTo(value, 0, 0)
-            : scroller.scroll.instance.scroll.y;
-    },
-    getBoundingClientRect() {
-        return {
-            left: 0,
-            top: 0,
-            width: window.innerWidth,
-            height: window.innerHeight
-        };
-    },
-    pinType: pageContainer.style.transform ? "transform" : "fixed"
-});
-
-
 window.addEventListener("load", function () {
-    let pinBoxes = document.querySelectorAll(".pin-wrap > *");
     let pinWrap = document.querySelector(".pin-wrap");
     let pinWrapWidth = pinWrap.offsetWidth;
     let horizontalScrollLength = pinWrapWidth - window.innerWidth;
 
-    // Pinning and horizontal scrolling
-
     gsap.to(".pin-wrap", {
         scrollTrigger: {
-            scroller: pageContainer, //locomotive-scroll
             scrub: true,
             trigger: "#sectionPin",
             pin: true,
-            // anticipatePin: 1,
+            anticipatePin: 1,
             start: "top top",
             end: pinWrapWidth
         },
         x: -horizontalScrollLength,
         ease: "none"
     });
-
-    ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
 
     ScrollTrigger.refresh();
 });
@@ -59,7 +26,7 @@ const animateit = function (e) {
     const hover = this.querySelector(".hover");
     const { offsetX: x, offsetY: y } = e;
     const { offsetWidth: width, offsetHeight: height } = this;
-    const move = 20;
+    const move = 30;
     const xMove = (x / width) * (move * 2) - move;
     const yMove = (y / height) * (move * 2) - move;
 
