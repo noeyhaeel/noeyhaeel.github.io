33f4439a4c0025bb06b1014756173d673a4513ce
eaahxn <alzl053@naver.com>
2023-12-28 10:47:54 +0900

    구조 변경

--- js/script.js
+++ js/script.js
@@ -1,34 +1,11 @@
-gsap.registerPlugin(ScrollTrigger);
-
-window.addEventListener("load", function () {
-    let pinWrap = document.querySelector(".pin-wrap");
-    let pinWrapWidth = pinWrap.offsetWidth;
-    let horizontalScrollLength = pinWrapWidth - window.innerWidth;
-
-    gsap.to(".pin-wrap", {
-        scrollTrigger: {
-            scrub: true,
-            trigger: "#sectionPin",
-            pin: true,
-            anticipatePin: 1,
-            start: "top top",
-            end: pinWrapWidth
-        },
-        x: -horizontalScrollLength,
-        ease: "none"
-    });
-
-    ScrollTrigger.refresh();
-});
-
-const link = document.querySelectorAll(".hover_link");
-const animateit = function (e) {
-    const hover = this.querySelector(".hover");
-    const { offsetX: x, offsetY: y } = e;
-    const { offsetWidth: width, offsetHeight: height } = this;
-    const move = 30;
-    const xMove = (x / width) * (move * 2) - move;
-    const yMove = (y / height) * (move * 2) - move;
+var link = document.querySelectorAll(".hover_link");
+var animateit = function (e) {
+    var hover = this.querySelector(".hover");
+    var { offsetX: x, offsetY: y } = e;
+    var { offsetWidth: width, offsetHeight: height } = this;
+    var move = 30;
+    var xMove = (x / width) * (move * 2) - move;
+    var yMove = (y / height) * (move * 2) - move;
 
     hover.style.transform = `translate(${xMove}px, ${yMove}px)`;
 
@@ -39,3 +16,13 @@ const animateit = function (e) {
 
 link.forEach((t) => t.addEventListener("mousemove", animateit));
 link.forEach((t) => t.addEventListener("mouseleave", animateit));
+
+window.onscroll = function () {
+    var ele = document.getElementById('pageBtn');
+    var distanceScrolled = document.documentElement.scrollTop;
+    if (distanceScrolled > 300) {
+        ele.classList.add('on');
+    } else {
+        ele.classList.remove('on');
+    }
+}
